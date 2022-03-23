55c190e412430f10766b9f809518d06c7dd1523e
Jacque Scriven <SCR16002083@btc.ac.uk>
2022-03-23 17:06:44 +0000

    Order controller updated to make sure only users with the customer role can access the checkout page and any other order controller pages.

--- OnlineShop2022/Controllers/OrderController.cs
+++ OnlineShop2022/Controllers/OrderController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using OnlineShop2022.Models;
 using Stripe;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace OnlineShop2022.Controllers
 {
+    [Authorize(Roles = "Customer")]
     public class OrderController : Controller
     {
         private readonly IOrderRepository _orderRepository;
